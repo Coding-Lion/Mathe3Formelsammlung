1ff3503fc98b06d98414432400053dd5cd77981b
Tim Hilt <timhilt@live.de>
2018-12-20 11:23:22 +0100

    Anhang ins Inhaltsverzeichnis eingefügt, kleine Änderung an Wahrsch.

--- auto/Mathe3Formelsammlung.el
+++ auto/Mathe3Formelsammlung.el
@@ -5,16 +5,19 @@
          "--shell-escape")
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("scrreprt" "12pt" "a4paper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("shorttoc" "tight")))
    (TeX-run-style-hooks
     "latex2e"
     "Title"
     "scrreprt"
     "scrreprt12"
-    "mystyle"
+    "shorttoc"
     "pgfplots"
     "amssymb"
     "todonotes"
-    "pdfpages")
+    "pdfpages"
+    "mystyle")
    (TeX-add-symbols
     "arraystretch"))
  :latex)
